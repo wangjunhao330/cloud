7d5f8281f1ba5deaae55a197000f9d8e0f7a8a77
wangjunhao <wangjunhao@migu.cn>
2019-12-27 15:50:26 +0800

    double eureka server

--- eureka-slave/src/main/java/com/wang/eurekaslave/EurekaSlaveApplication.java
+++ eureka-slave/src/main/java/com/wang/eurekaslave/EurekaSlaveApplication.java
@@ -1,15 +1,16 @@
-package com.wang.eurekaserver;
+package com.wang.eurekaslave;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaServerApplication {
+public class EurekaSlaveApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
+        SpringApplication.run(EurekaSlaveApplication.class, args);
     }
 
 }
