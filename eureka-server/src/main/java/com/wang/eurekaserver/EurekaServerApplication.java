ee5c554d686550f743e90038e0f73184e82ddd07
wangjunhao <wangjunhao@migu.cn>
2019-12-27 14:04:29 +0800

    init project version 1

---
+++ eureka-server/src/main/java/com/wang/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.wang.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringCloudApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
---
+++ eureka-server/src/test/java/com/wang/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wang.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
